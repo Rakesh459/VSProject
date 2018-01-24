4e0d4f964b585ba14a95cf24697bd9ab0d6b51ae
Sanna <Sanna@gmail.com>
2018-01-24 21:12:17 +0530

    Initial Project

---
+++ GlobalErrorHandler/GlobalErrorHandler/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace GlobalErrorHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(HelloService)))
+            {
+                host.Open();
+                Console.WriteLine("host Started");
+                Console.ReadKey();
+            }
+        }
+    }
+}
