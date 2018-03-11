66c22cf578d064d933636df0cfd1f62b96f0121f
Rakesh459 <rakeshkumarreddy907@gmail.com>
2018-03-11 17:39:08 +0530

    rebase 2

--- GlobalErrorHandler/GlobalErrorHandler/Program.cs
+++ GlobalErrorHandler/GlobalErrorHandler/Program.cs
@@ -11,6 +11,7 @@ namespace GlobalErrorHandler
         static void Main(string[] args)
         {
             // added for merge conflict
+            //added to make merge conflict to check rebase
             //added to make conflict and check how rebase works
             //comment to understand
             //always comment to understand
